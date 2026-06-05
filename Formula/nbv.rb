4c5b31d675ca2f7549b8399805f2d5e16fede1ed
phd1000x-ux <phd1000x@gmail.com>
2026-06-05 13:53:47 +0900

    nbv: bump to v0.6.1

--- Formula/nbv.rb
+++ Formula/nbv.rb
@@ -5,8 +5,8 @@ class Nbv < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/phd1000x-ux/nbv/releases/download/v0.6.0/nbv-v0.6.0-aarch64-apple-darwin.tar.gz"
-      sha256 "317e39efb5bdaed8219ec661a75f6a61017218b568872c5e24e4ce073de50a22"
+      url "https://github.com/phd1000x-ux/nbv/releases/download/v0.6.1/nbv-v0.6.1-aarch64-apple-darwin.tar.gz"
+      sha256 "27e13d597f9b4db1124bb1e26b73104d5549f303b0d1775901003df917a0c2b4"
     end
   end
 
